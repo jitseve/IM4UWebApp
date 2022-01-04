98174ab4bcfa18e4c05485377662e131f7663d1f
jitseve <jj.ve.student@gmail.com>
2022-01-04 12:33:36 +0100

    Added step frequency detection

--- processing/settings.py
+++ processing/settings.py
@@ -22,3 +22,16 @@ class Settings(object):
 
     # IMU settings
     f_sampling = 1/218
+
+    def stepRegisterInit():
+        K0 = 300
+        alpha = 0.7
+        W2 = 3
+        TH_pk = 25
+        TH_s = 200
+
+        TH = 6
+        W1 = 3
+        TH_vy = 15
+
+        return K0, alpha, W2, TH_pk, TH_s, TH, W1, TH_vy
